512d4b02cbe6af3e8e4642f64ec11e6ae061b493
Andrey Tanakov <andrey.tanakov@ispring.com>
2025-05-02 14:15:18 +0300

    Сгенерированный проект с KMP.

---
+++ otus-ios-hw-12/shared/src/commonMain/kotlin/com/example/otusioshw12/Greeting.kt
@@ -0,0 +1,9 @@
+package com.example.otusioshw12
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
